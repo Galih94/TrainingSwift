4c2f9fd31ffb99457e709f517c17f9f8ce1d05f6
galih <samudragalih94@gmail.com>
2024-12-15 14:08:47 +0700

    feat(Int): Integer type propertu explanation

--- Training.playground/Contents.swift
+++ Training.playground/Contents.swift
@@ -4,3 +4,9 @@ import Cocoa
 var greeting = "Hello, playground"
 greeting = "Hallo"
 /// ---
+/// `Int` or Integer use to store number, for large number using `_` to separate number is ok and not change the value of `Int`
+var age: Int = 25
+var priceUnderScore = 10_000_000
+var price = 10000000
+print("prices are \(priceUnderScore) and \(price)")
+/// ---
